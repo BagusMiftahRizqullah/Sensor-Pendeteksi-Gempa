d26bc7445d865bf29243917f8870318184281a82
Miftah <37774184+BagusMiftahRizqullah@users.noreply.github.com>
2020-06-22 19:57:13 +0800

    Update
    penambahan comment

--- sensor_getar_gempa.ino
+++ sensor_getar_gempa.ino
@@ -1,3 +1,5 @@
+// create by : Bagus Miftah Rizqullah
+
 const int vibrationSensorPin = 2; // Vibration Sensor di hubungkan ke Pin 2
 int led = 13;                // the pin that the LED is atteched to
 int vibrationSensorState = 0; // Status saat pertama mulai = 0
@@ -12,7 +14,7 @@ void setup() {
   pinMode(led, OUTPUT);      // initalize LED as an output
  // CODINGAN RELAY aktif
   pinMode(pinRelay,OUTPUT);          // definisikan keluaran
-  digitalWrite(pinRelay,HIGH); // nyalain relay
+  digitalWrite(pinRelay,HIGH); // matikan relay
   digitalWrite(led, HIGH);   // turn LED ON
   Serial.begin(115200);        // initialize serial
   pinMode(vibrationSensorPin, INPUT); // Jadikan Vibration sensor sebagai input
@@ -28,7 +30,7 @@ vibrationSensorState = digitalRead(vibrationSensorPin);
 if (vibrationSensorState == HIGH) { // Jika ada getaran di sensor = HIGH
       Serial.println("Ada Getaran!");
       digitalWrite(pinRelay,LOW); // nyalain relay
-      delay(6000);                // delay 100 milliseconds
+      delay(6000);                // delay 6 detik
       Serial.println("MATIKAN ALARAM !!!");
       digitalWrite(led, HIGH); // turn LED OFF
       digitalWrite(pinRelay,HIGH); // matikan relay
@@ -38,7 +40,7 @@ else {
 
       Serial.println("Tidak Ada Getaran!");
       digitalWrite(led, HIGH); // turn LED OFF
-      delay(1000);
+      delay(1000);             // delay 6 detik
 
 }
 
